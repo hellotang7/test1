d044938bd4a1456f27238ab6203f4a4e956f2717
hellotang7 <2714148252@qq.com>
2023-12-29 16:00:58 +0800

    搭建萤石云中

--- src/components/Tags/Equipment/EzvizLive.vue
+++ src/components/Tags/Equipment/EzvizLive.vue
@@ -7,35 +7,36 @@
 
  const route = useRoute()
 
-  const EZUIKitPlayer = shallowRef({
-    player:null
-  });
+  // const EZUIKitPlayer = shallowRef({
+  //   player:null
+  // });
 
   onMounted(()=>{
     const no = route.query.no;
     getEzvizLive(no as any).then((res)=>{
       if(res.data.success){
-        EZUIKitPlayer.value.player = new EZUIKit.EZUIKitPlayer({
+        // alert( res.data.data.url)
+        // alert( res.data.data.accessToken)
+        const EZUIKitPlayer = new EZUIKit.EZUIKitPlayer({
           id: `ezuikit-player`,
           url: res.data.data.url,
           accessToken:res.data.data.accessToken,
-
         })
       }
     })
   })
 
-  onUnmounted(()=>{
-    console.info(EZUIKitPlayer.value.player.stop());
-  })
-
-  const handleResize = (dom:HTMLElement) => {
-    EZUIKitPlayer.value.player.reSize(dom.clientWidth,dom.clientHeight)
-  }
+  // onUnmounted(()=>{
+  //   console.info(EZUIKitPlayer.value.player.stop());
+  // })
+  //
+  // const handleResize = (dom:HTMLElement) => {
+  //   EZUIKitPlayer.value.player.reSize(dom.clientWidth,dom.clientHeight)
+  // }
 </script>
 
 <template>
-  <div class="main-content" v-resize="handleResize">
+  <div class="main-content" >
     <div id="ezuikit-player"></div>
   </div>
 </template>
